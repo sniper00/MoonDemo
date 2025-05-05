68280f8f1f6ddae7d62d2deffdf945e4b8daa497
bruce <hanyongtao@live.com>
2025-05-05 11:59:54 +0800

    Update start scripts

--- server/start_server.bat
+++ server/start_server.bat
@@ -1,6 +1,8 @@
 IF EXIST moon\moon.exe (
     start cmd /k moon\moon.exe main_hub.lua 10000 node.json
 ) ELSE (
+    cd moon && premake5 build --release
+    cd ..
     start cmd /k "cd moon && premake5 run --release ..\main_hub.lua 10000 node.json"
 )
 timeout /t 1
